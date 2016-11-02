d41b31f867401ed46173e91faa95f3dd878e56c0
chrisbacon <bacon.chris@gmail.com>
2016-11-02 14:35:08 +0000

    created first test

--- tdd_array_loop_hash_lab_spec_start_point.rb
+++ tdd_array_loop_hash_lab_spec_start_point.rb
@@ -4,7 +4,10 @@ require_relative('./tdd_array_loop_hash_start_point_lab')
 class Lab < MiniTest::Test
 
   def test_add_array_lengths
-
+    prices = [1.23, 6.98, 8.43, 2.45]
+    costs = [4.23, 1.12, 0.52, 8.67]
+    result = add_array_lengths(prices, cost)
+    assert_equals(8, result)
   end
 
 
