f27713cd05bf73a2e6eef33f3d16ccde4c1e0cfd
chrisbacon <bacon.chris@gmail.com>
2016-11-02 14:46:36 +0000

    second function tested

--- tdd_array_loop_hash_lab_start_point.rb
+++ tdd_array_loop_hash_lab_start_point.rb
@@ -2,3 +2,11 @@ def add_array_lengths(array1, array2)
   result = array1.length + array2.length
   return result
 end
+
+def sum_array(array)
+  sum = 0
+  for number in array
+    sum = sum + number
+  end
+  return sum
+end
