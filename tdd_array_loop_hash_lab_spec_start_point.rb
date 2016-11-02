546fc703e9e0cfab15202039d07d1e391e8bd77f
chrisbacon <bacon.chris@gmail.com>
2016-11-02 14:31:24 +0000

    initial commit

---
+++ tdd_array_loop_hash_lab_spec_start_point.rb
@@ -0,0 +1,30 @@
+require('minitest/autorun')
+require_relative('./tdd_array_loop_hash_start_point_lab')
+
+class Lab < MiniTest::Test
+
+  def test_add_array_lengths
+
+  end
+
+
+  def test_sum_array
+
+  end
+
+
+  def test_find_item
+
+  end
+
+
+  def test_first_key_name
+
+  end
+
+
+  def test_array_of_capitals
+
+  end
+
+end
